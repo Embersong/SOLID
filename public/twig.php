7722050b5e358d3992f1200fea9b449341730ef2
Embersong <evebay@mail.ru>
2020-12-08 17:18:14 +0700

    Delete twig.php

--- public/twig.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('../twigtemplates');
-$twig = new \Twig\Environment($loader);
-
-echo $twig->render('index.twig', [
-    'name' => 'Admin'
-]);
